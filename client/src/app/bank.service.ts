b2990abfe41fa9b85de437deadf1cf53307b20d8
litalrab <38513468+litalrab@users.noreply.github.com>
2020-01-19 00:29:49 +0200

    Update bank.service.ts

--- client/src/app/bank.service.ts
+++ client/src/app/bank.service.ts
@@ -8,7 +8,9 @@ export class BankService {
   namesList;
   branchNumbers: any;
   bankDetails: any;
-
+  srcMap;
+  errorInform: boolean;
+  errMsg: string;
   constructor(private httpClient: HttpClient) { }
 
   getBankNames(char) {
@@ -16,22 +18,37 @@ export class BankService {
       .subscribe((res) => {
         console.log(new Set([].concat(...Object.values(res))));
         this.namesList = Array.from(new Set([].concat(...Object.values(res)))); // remove dup
+
       },
-      (err) => {console.log( err)}         );
+        (err) => {
+      
+          console.log(err)
+        });
   }
 
   getBranchNums(bankName) {
     this.httpClient.get('http://localhost:3000/branch/' + bankName)
-      .subscribe((res) => { this.branchNumbers = res;  },
-       (err) => {console.log(err)}         
-      ) ;
+      .subscribe((res) => { this.branchNumbers = res; },
+        (err) => { console.log(err) }
+      );
   }
   getDetails(bankName, branchNumber) {
     return this.httpClient.get(`http://localhost:3000/details/${bankName}/${branchNumber}`)
       .subscribe((res) => {
         this.bankDetails = res[0];
-        console.log(this.bankDetails)
+        let address = this.bankDetails.City.toString() + " " + this.bankDetails.Branch_Address.toString()
+        console.log(this.bankDetails.City.toString().replace(" ", "%20"))
+        this.srcMap = `https://maps.google.com/maps?q=${address}&t=&output=embed`
+        console.log(this.srcMap)
+
+        this.errorInform = false;
+
+
       },
-      (err) => {console.log(err)}         );
+        (err) => {
+          console.log(err)
+          this.errorInform = true;
+          this.errMsg = "invalid bank number"
+        });
   }
 }
