57f24c32d735ed2b081a8ec0e7470567aaf1ea1c
litalrab <38513468+litalrab@users.noreply.github.com>
2020-01-19 00:30:55 +0200

    Update bank.component.ts

--- client/src/app/bank/bank.component.ts
+++ client/src/app/bank/bank.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgZone, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { BankService } from '../bank.service';
+import { MapsAPILoader, AgmMap } from '@agm/core';
+import { GoogleMapsAPIWrapper } from '@agm/core/services';
+import {DomSanitizer} from '@angular/platform-browser'
+declare var google: any;
 
+ 
 @Component({
   selector: 'app-bank',
   templateUrl: './bank.component.html',
@@ -15,9 +20,12 @@ export class BankComponent implements OnInit {
   branchNumber
   isFormfull;
   bankDetails;
-  errorInform;
-  errMsg: string;
-  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private bankService: BankService
+
+
+
+
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone,public sanitizer:DomSanitizer, private bankService: BankService
   ) {
     this.bankName = '';
     this.isFormfull = false;
@@ -35,15 +43,24 @@ export class BankComponent implements OnInit {
     this.bankName = e.target.value;
     if (this.bankName)
       this.bankService.getBranchNums(this.bankName);
+      if(!this.bankService.namesList)
+      {
+        this.bankService.errorInform = true;
+        this.bankService.errMsg = "invalid bank name"
+      }
+      else
+      {
+        this.bankService.errorInform = false;
 
+      }
   }
   chooseBranchNum(e) {
     this.branchNumber = e.target.value;
     if (this.bankName && this.branchNumber)
       this.isFormfull = true;
     else {
-      this.errorInform = true;
-      this.errMsg = "please fill in name first"
+      this.bankService.errorInform = true;
+      this.bankService.errMsg = "please fill in name first"
 
     }
   }
@@ -52,27 +69,15 @@ export class BankComponent implements OnInit {
     let char = event.target.value;
     if (char)
       this.bankService.getBankNames(char);
-    if (!this.bankService.namesList) {
-      this.errorInform = true;
-      this.errMsg = "invalid bank name"
-    }
-    else {
-      this.errorInform = false;
-    }
+
   }
 
   onSubmit() {
     this.isFormfull = false;
     if (this.bankName && this.branchNumber)
       this.bankService.getDetails(this.bankName, this.branchNumber)[0];
-      if (!this.bankService.bankDetails) {
-        this.errorInform = true;
-        this.errMsg = "invalid bank number"
-      }
-      else {
-        this.errorInform = false;
-      }
-      // this.httpClient.get(`http://localhost:3000/details/${this.bankName}/${this.branchNumber}`)
+    
+    // this.httpClient.get(`http://localhost:3000/details/${this.bankName}/${this.branchNumber}`)
     //   .subscribe((res) => {
     //     this.bankDetails = res[0];
     //     console.log(this.bankDetails)
